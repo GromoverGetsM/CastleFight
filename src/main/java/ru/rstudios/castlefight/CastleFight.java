38efb2599bb76f38d7b0a8fa7f55404bffc97d7c
GromoverGetsM <miroshkin.kirill09@gmail.com>
2024-06-07 22:06:01 +0300

    Основные изменения || 22:00 07.06.2024 MSK

--- src/main/java/ru/rstudios/castlefight/CastleFight.java
+++ src/main/java/ru/rstudios/castlefight/CastleFight.java
@@ -1,14 +1,17 @@
 package ru.rstudios.castlefight;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.rstudios.castlefight.utils.FileUtil;
 
 public final class CastleFight extends JavaPlugin {
 
     public static JavaPlugin plugin;
+    public static FileUtil fileUtil;
 
     @Override
     public void onEnable() {
         plugin = this;
+        fileUtil = new FileUtil();
     }
 
     @Override
--- src/main/java/ru/rstudios/castlefight/utils/FileUtil.java
+++ src/main/java/ru/rstudios/castlefight/utils/FileUtil.java
@@ -4,9 +4,10 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 import static ru.rstudios.castlefight.CastleFight.plugin;
-
+import static ru.rstudios.castlefight.CastleFight.fileUtil;
 public class FileUtil {
     public FileConfiguration loadFile (String fileName) {
         return YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), fileName));
@@ -14,5 +15,13 @@ public class FileUtil {
 
     public void saveUnusualConfig (String fileName, boolean needsToReplace) {
         plugin.saveResource(fileName, needsToReplace);
+    }
+
+    public void save (String fileName) throws IOException {
+        if (fileName.equalsIgnoreCase("config.yml")) {
+            plugin.saveConfig();
+        } else {
+            fileUtil.loadFile(fileName).save(new File(plugin.getDataFolder(), fileName));
+        }
     }
 }
