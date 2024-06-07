77d1a9b810dc32fb44e619d2e8749c598767b08b
BuildTools <unconfigured@null.spigotmc.org>
2024-06-07 22:09:16 +0300

    ModeUtil

--- src/main/java/ru/rstudios/castlefight/utils/GameModeUtil.java
+++ src/main/java/ru/rstudios/castlefight/utils/GameModeUtil.java
@@ -2,20 +2,29 @@ package ru.rstudios.castlefight.utils;
 
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class GameModeUtil implements Listener {
 
     GameMode mode = null;
 
     public void SetMode(String modeset, Player player) {
-        if (modeset == "1") {
+        if (modeset == "c") {
             GameMode mode = GameMode.CREATIVE;
-        } else if (modeset == "2") {
+        } else if (modeset == "a") {
             GameMode mode = GameMode.ADVENTURE;
-        } else if (modeset == "3") {
+        } else if (modeset == "s") {
             GameMode mode = GameMode.SPECTATOR;
         }
         player.setGameMode(mode);
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player pl = e.getPlayer();
+        SetMode("c",pl);
+    }
+
 }
